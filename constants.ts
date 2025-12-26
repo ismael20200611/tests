87fa9cf1f47aadd7e136aea15755c03021f3e7a3
ismael20200611 <ismael20200611@gmail.com>
2025-12-26 06:28:41 +0000

    Add files via upload

--- constants.ts
+++ constants.ts
@@ -1,3 +1,4 @@
+
 export const CATEGORIES = ['All', 'Shawarma', 'Pizza', 'Grill', 'Platter', 'Sides', 'Drinks'];
 
 export const MENU_ITEMS = [
@@ -20,5 +21,5 @@ export const MENU_ITEMS = [
 
 export const TABLES = Array.from({ length: 30 }, (_, i) => `T${i + 1}`);
 export const USERS = ['user1', 'user2', 'user3', 'admin'];
-export const CONTACT_NUMBER = '+96877611327';
+export const CONTACT_NUMBER = '+447400123456';
 export const COMPANY_EMAIL = 'fastfood@gmail.com';
